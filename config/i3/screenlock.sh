b8e23f01ee883ec576f4df4f75f047a594d468e2
Guillaume Gauvrit <guillaume@gandi.net>
2018-09-18 11:59:56 +0200

    Update screenlock for better usability

--- config/i3/screenlock.sh
+++ config/i3/screenlock.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-i3lock -c 080808
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 888x1080 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
+i3lock -c 160535
