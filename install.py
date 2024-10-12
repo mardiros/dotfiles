23a237ff1316c5f61f648e8639c46dabd2597067
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-12 14:26:32 +0200

    Implement a --reverse command for the install.py file

--- install.py
+++ install.py
@@ -35,111 +35,139 @@ import os.path
 import shutil
 import sys
 import time
+from typing import Tuple
 
 sep = os.path.sep
 curfile = os.path.abspath(__file__)
 here = os.path.dirname(curfile) + sep
-dest = os.path.expanduser('~') + sep + '.'
+dest = os.path.expanduser("~") + sep + "."
 
 
-def identical(src, target):
+def identical(src: str, target: str) -> Tuple[bool, str]:
     ident = True
-    sdiff = ''
+    sdiff = ""
     with open(src) as fsrc:
         with open(target) as ftarget:
             diff = difflib.ndiff(ftarget.readlines(), fsrc.readlines())
 
             diff_header = True
             for idx, line in enumerate(diff):
-                if line.startswith(' '):
+                if line.startswith(" "):
                     diff_header = True
                     continue
                 if diff_header:
-                   diff_header = False
-                   if ident:
-                       ident = False
-                   sdiff += '{} +{}\n'.format(target, idx)
+                    diff_header = False
+                    if ident:
+                        ident = False
+                    sdiff += "{} +{}\n".format(target, idx)
                 sdiff += line
     if not ident:
-        sdiff += '\n'
+        sdiff += "\n"
     return ident, sdiff
 
-def main(dry_run, verbose):
 
+def main(dry_run: bool, verbose: bool, reverse: bool) -> None:
     for root, dirs, files in os.walk(here):
-         root = root.rstrip(sep) + sep
-         prefix = root.replace(here, dest)
+        root = root.rstrip(sep) + sep
+        prefix = root.replace(here, dest)
 
-         for src in dirs:
-             if (root + src).startswith(here + '.'):
-                 if verbose:
-                     print('Ignoring hidden directory {}'.format(root + src))
-                 continue
+        for src in dirs:
+            if (root + src).startswith(here + "."):
+                if verbose:
+                    print("Ignoring hidden directory {}".format(root + src))
+                continue
 
-             target = prefix + src
-             if os.path.exists(target):
-                 if os.path.isdir(target):
-                     if verbose:
-                         print('Ignoring existing directory {}'.format(target))
-                     continue
+            target = prefix + src
+            if os.path.exists(target):
+                if os.path.isdir(target):
+                    if verbose:
+                        print("Ignoring existing directory {}".format(target))
+                    continue
 
-                 print('Remove file {}, replaced by a src'.format(target))
-                 if not dry_run:
-                     os.remove(target)
+                print("Remove file {}, replaced by a src".format(target))
+                if not dry_run:
+                    os.remove(target)
 
-             print('Creating new src {}'.format(target))
-             if not dry_run:
-                 os.mkdir(target, mode=0o700)
+            print("Creating new src {}".format(target))
+            if not dry_run:
+                os.mkdir(target, mode=0o700)
 
-         for src in files:
-             if src.startswith('.') or \
-                     (root + src).startswith(here + '.') or \
-                     root + src == curfile or \
-                     root + src == here + 'LICENSE' or \
-                     root + src == here + 'README.md':
-                 if verbose:
-                     print('Ignored file {}'.format(root + src))
-                 continue
-             target = sep.join([prefix + src])
-             src = root + src
-             if os.path.exists(target):
-                 if verbose:
-                     print('Comparing files {} {}'.format(src, target))
-                 try:
-                     ident, diff = identical(src, target)
-                 except UnicodeDecodeError:
-                     diff = '[binary files]'
-                     src_time = time.ctime(os.path.getmtime(src))
-                     target_time = time.ctime(os.path.getmtime(target))
-                     ident = src_time == target_time
-                 if ident:
-                     if verbose:
-                         print('Ignoring identical file {}'.format(target))
-                 else:
-                     print('Replacing {} by {}'.format(target, src))
-                     if verbose or dry_run:
-                         print(diff)
-             else:
-                 print('Copying {} to {}'.format(src, target))
-             if not dry_run:
-                 shutil.copy2(src, target)
+        for src in files:
+            if (
+                src.startswith(".")
+                or (root + src).startswith(here + ".")
+                or root + src == curfile
+                or root + src == here + "LICENSE"
+                or root + src == here + "README.md"
+            ):
+                if verbose:
+                    print("Ignored file {}".format(root + src))
+                continue
+            target = sep.join([prefix + src])
+            src = root + src
+            if os.path.exists(target):
+                if verbose:
+                    print("Comparing files {} {}".format(src, target))
+                try:
+                    if reverse:
+                        ident, diff = identical(target, src)
+                    else:
+                        ident, diff = identical(src, target)
+                except UnicodeDecodeError:
+                    diff = "[binary files]"
+                    src_time = time.ctime(os.path.getmtime(src))
+                    target_time = time.ctime(os.path.getmtime(target))
+                    ident = src_time == target_time
+                if ident:
+                    if verbose:
+                        print("Ignoring identical file {}".format(target))
+                else:
+                    if reverse:
+                        print("Replacing {} by {}".format(src, target))
+                    else:
+                        print("Replacing {} by {}".format(target, src))
+                    if verbose or dry_run:
+                        print(diff)
+            else:
+                if reverse:
+                    print("Copying {} to {}".format(target, src))
+                else:
+                    print("Copying {} to {}".format(src, target))
+            if not dry_run:
+                if reverse:
+                    shutil.copy2(target, src)
+                else:
+                    shutil.copy2(src, target)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose',
-                        dest='verbose',
-                        action='store_const',
-                        const=True,
-                        default=False)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_const",
+        const=True,
+        default=False,
+    )
 
-    parser.add_argument('-d', '--dry-run',
-                        dest='dry_run',
-                        action='store_const',
-                        const=True,
-                        default=False)
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        dest="dry_run",
+        action="store_const",
+        const=True,
+        default=False,
+    )
+
+    parser.add_argument(
+        "-r",
+        "--reverse",
+        dest="reverse",
+        action="store_const",
+        const=True,
+        default=False,
+    )
 
     kwargs = vars(parser.parse_args(sys.argv[1:]))
     main(**kwargs)
-
